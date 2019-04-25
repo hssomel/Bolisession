ccb49101c22bd1043dbc07455278c347458e6a7a
Gurkamal Singh <gurkamalpsc@gmail.com>
2019-04-24 22:24:14 -0700

    more linting with eslint

--- server/src/utils/requestLogger.js
+++ server/src/utils/requestLogger.js
@@ -1,17 +1,17 @@
-module.exports = (req, res) => {
-    const { 
-        method, 
-        originalUrl, 
-        protocol, 
-        ip, 
-        body,
-        params,
-        query,
-        headers
-    } = req;
-    console.log(`\n${method} "${originalUrl}" from ${protocol}://${ip}`);
-    console.log(`req.body = `, body);
-    console.log(`req.params = `, params);
-    console.log(`req.query = `, query);
-    console.log(`req.headers = `, headers);
+module.exports = req => {
+  const {
+    method,
+    originalUrl,
+    protocol,
+    ip,
+    body,
+    params,
+    query,
+    headers,
+  } = req;
+  console.log(`\n${method} "${originalUrl}" from ${protocol}://${ip}`);
+  console.log(`req.body = `, body);
+  console.log(`req.params = `, params);
+  console.log(`req.query = `, query);
+  console.log(`req.headers = `, headers);
 };
