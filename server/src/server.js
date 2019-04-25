ebb6d5f028319ad0452b417e534897262f216bdd
Gurkamal Singh <gurkamalpsc@gmail.com>
2019-04-24 22:11:32 -0700

    linted with esLint

--- server/src/server.js
+++ server/src/server.js
@@ -1,23 +1,24 @@
-require("dotenv").config(); // sets environment variables
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
-const keys = require("./config/keys");
-const ip = require("ip");
-const fs = require("fs");
+require('dotenv').config(); // sets environment variables
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const ip = require('ip');
+const fs = require('fs');
+const http = require('http');
+const keys = require('./config/keys');
 
 // Import API route files
-const credentials = require("./routes/api/credentials");
-const profiles = require("./routes/api/profiles");
-const posts = require("./routes/api/posts");
+const credentials = require('./routes/api/credentials');
+const profiles = require('./routes/api/profiles');
+const posts = require('./routes/api/posts');
 
 // Initialize express
 const app = express();
-const server = require('http').createServer(app);
+const server = http.createServer(app);
 
 // Initialize socket.io
-require("./sockets/socketServer")(server);
+require('./sockets/socketServer')(server);
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,10 +28,10 @@ app.use(bodyParser.json());
 const address = ip.address();
 const port = process.env.PORT || 8080;
 const url = `http://${address}:${port}`;
-const requestLogger = require("./utils/requestLogger");
+const requestLogger = require('./utils/requestLogger');
 
 // Test Response at API root route; Log any errors
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   requestLogger(req, res);
   res.json({ msg: `API connected at ${url}` });
 });
@@ -38,30 +39,30 @@ app.get("/", (req, res) => {
 // Connect to MongoDB
 mongoose
   .connect(keys.mongoURI, { useNewUrlParser: true, useCreateIndex: true })
-  .then(() => console.log("MongoDB Connected"))
+  .then(() => console.log('MongoDB Connected'))
   .catch(error => console.log(error));
 
 // Passport middleware
 app.use(passport.initialize());
 
-//Passport Config
-require("./config/passport")(passport);
+// Passport Config
+require('./config/passport')(passport);
 
 // Use Routes
-app.use("/api/credentials", credentials);
-app.use("/api/profiles", profiles);
-app.use("/api/posts", posts);
+app.use('/api/credentials', credentials);
+app.use('/api/profiles', profiles);
+app.use('/api/posts', posts);
 
 // Write a env.js file in the client directory to configure API_BASE_URL
 const data = `export const API_BASE_URL = "http://${ip.address()}:${port}";`;
 // const data = `export const API_BASE_URL = "http://localhost:${port}";`;
-fs.writeFile("../mobile/env.js", data, err => {
-  if (err) console.log("Error while writing client env.js file", err);
-  else console.log("Generated MOBILE env.js configuration!");
+fs.writeFile('../mobile/env.js', data, err => {
+  if (err) console.log('Error while writing client env.js file', err);
+  else console.log('Generated MOBILE env.js configuration!');
 });
-fs.writeFile("../web/src/env.js", data, err => {
-  if (err) console.log("Error while writing client env.js file", err);
-  else console.log("Generated WEB env.js configuration!");
+fs.writeFile('../web/src/env.js', data, err => {
+  if (err) console.log('Error while writing client env.js file', err);
+  else console.log('Generated WEB env.js configuration!');
 });
 
 // Run Server on that Port
