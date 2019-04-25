4d3800215be670b7157263a89a631558ab736c4b
Gurkamal Singh <gurkamalpsc@gmail.com>
2019-04-25 00:58:54 -0700

    deleted more duplicate files

--- server/src/utils/requestLogger.js
+++ server/src/utils/requestLogger.js
@@ -1,17 +1,17 @@
 module.exports = req => {
-  //   const {
-  //     method,
-  //     originalUrl,
-  //     protocol,
-  //     ip,
-  //     body,
-  //     params,
-  //     query,
-  //     headers,
-  //   } = req;
-  //   console.log(`\n${method} "${originalUrl}" from ${protocol}://${ip}`);
-  //   console.log(`req.body = `, body);
-  //   console.log(`req.params = `, params);
-  //   console.log(`req.query = `, query);
-  //   console.log(`req.headers = `, headers);
+  const {
+    method,
+    originalUrl,
+    protocol,
+    ip,
+    body,
+    params,
+    query,
+    headers,
+  } = req;
+  console.log(`\n${method} "${originalUrl}" from ${protocol}://${ip}`);
+  console.log(`req.body = `, body);
+  console.log(`req.params = `, params);
+  console.log(`req.query = `, query);
+  console.log(`req.headers = `, headers);
 };
