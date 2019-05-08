35ec02a40b2de2f8d6040bf499acd52fb0ce73a8
Gurkamal Singh <gurkamalpsc@gmail.com>
2019-05-08 04:14:17 -0700

    redid the firebase integration - cocoapods gives issues if left to tits own vices.......all native integration should be done by renaming Podfile to fake name and running react native link unless cocoapods is specifically used

--- client/android/app/src/main/java/com/bolisession/bolisession/MainActivity.java
+++ client/android/app/src/main/java/com/bolisession/bolisession/MainActivity.java
@@ -2,23 +2,24 @@ package com.bolisession.bolisession;
 
 import com.facebook.react.ReactActivity;
 
-// For react-native-gesture-handler
+// for react-native-gesture-handler ////////////////////////////////////////////
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+////////////////////////////////////////////////////////////////////////////////
 
 public class MainActivity extends ReactActivity {
 
     /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
+     * Returns the name of the main component registered from JavaScript. This is
+     * used to schedule rendering of the component.
      */
     @Override
     protected String getMainComponentName() {
         return "bolisession";
     }
-    
-    // For react-native-gesture-handler
+
+    // for react-native-gesture-handler ////////////////////////////////////////
     @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
         return new ReactActivityDelegate(this, getMainComponentName()) {
@@ -28,4 +29,5 @@ public class MainActivity extends ReactActivity {
             }
         };
     }
+    /////////////////////////////////////////////////////////////////////////////
 }
