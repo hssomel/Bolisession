216fb300b1000f54c57289af78fe0439dd45dbbe
hssomel <somel.harpreet@gmail.com>
2019-05-22 22:14:35 -0700

    updated CreateAccountScreen

--- client/src/screens/CreateAccountScreen.js
+++ client/src/screens/CreateAccountScreen.js
@@ -4,26 +4,25 @@ import {
   Picker,
   SafeAreaView,
   TextInput,
-  TouchableOpacity,
   Image,
   Text,
   ScrollView,
+  View,
 } from 'react-native';
+import { Button } from 'react-native-elements';
+import LinearGradient from 'react-native-linear-gradient';
 
 export default function CreateAccountScreen(props) {
   // Initial State
-  const [textInputStyle, setTextInputStyle] = useState(
-    // borderBottomWidth: 2,
-    // borderBottomColor: 'red',
-    // fontSize: 20,
-    // width: '80%',
-    // paddingBottom: '-1%',
-    '50%',
-  );
+  const [textInputStyle, setTextInputStyle] = useState('50%');
   const [currentTeam, setCurrentTeam] = useState('');
+  const [focusOn, setFocusOn] = useState(false);
 
   // Event Handlers
-  const handleOnFocus = () => setTextInputStyle('20%');
+  const handleOnFocus = () => {
+    setTextInputStyle('20%');
+    setFocusOn(true);
+  };
   const handleOnScroll = () => setTextInputStyle('50%');
   const handleOnBlur = () => setTextInputStyle('50%');
 
@@ -41,28 +40,44 @@ export default function CreateAccountScreen(props) {
         />
         <Text style={styles.text}>Create your account</Text>
         <TextInput
-          placeholder={'Username'}
+          placeholder="Username"
           onFocus={handleOnFocus}
           onScroll={handleOnScroll}
           onBlur={handleOnBlur}
           style={{
-            borderBottomWidth: 2.2,
-            borderBottomColor: 'red',
-            fontSize: 20,
-            width: '80%',
-            paddingBottom: '-1%',
+            ...styles.textInput,
             marginTop: textInputStyle,
           }}
         />
-        <Picker
-          selectedValue={currentTeam}
-          style={styles.picker}
-          onValueChange={itemValue => setCurrentTeam(itemValue)}
-        >
-          <Picker.Item label="Free Agent" value="Free Agent" />
-          <Picker.Item label="Bhangra Empire" value="Bhangra Empire" />
-          <Picker.Item label="UCR Bhangra" value="UCR Bhangra" />
-        </Picker>
+        <View style={styles.container2}>
+          <Picker
+            selectedValue={currentTeam}
+            style={styles.picker}
+            onValueChange={itemValue => setCurrentTeam(itemValue)}
+          >
+            <Picker.Item label="Free Agent" value="Free Agent" />
+            <Picker.Item label="Bhangra Empire" value="Bhangra Empire" />
+            <Picker.Item label="UCR Bhangra" value="UCR Bhangra" />
+            <Picker.Item
+              label="Apni Sardari Apni Pechaan"
+              value="Apni Sardari Apni Pechaan"
+            />
+          </Picker>
+        </View>
+        {focusOn && (
+          <Button
+            containerStyle={styles.buttonContainer}
+            buttonStyle={styles.buttonStyle}
+            ViewComponent={LinearGradient}
+            linearGradientProps={{
+              colors: ['red', 'orange'],
+              start: { x: 0, y: 0.5 },
+              end: { x: 1, y: 0.5 },
+            }}
+            title="CONTINUE"
+            fontSize={38}
+          />
+        )}
       </ScrollView>
     </SafeAreaView>
   );
@@ -85,6 +100,16 @@ const styles = StyleSheet.create({
     width: '100%',
     flex: 1,
   },
+  container2: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    height: '50%',
+    width: '83%',
+    flex: 1,
+    borderBottomColor: 'red',
+    borderBottomWidth: 2,
+  },
   image: {
     height: 60,
     width: 60,
@@ -97,16 +122,30 @@ const styles = StyleSheet.create({
     color: 'black',
     marginTop: '8%',
   },
-  //   textInput: {
-  //     borderBottomWidth: 2,
-  //     borderBottomColor: 'red',
-  //     fontSize: 20,
-  //     width: '80%',
-  //     paddingBottom: '-1%',
-  //   },
+  textInput: {
+    borderBottomWidth: 2,
+    borderBottomColor: 'red',
+    fontSize: 20,
+    width: '83%',
+    paddingBottom: '-1%',
+  },
   picker: {
-    width: '85%',
+    width: '105%',
     height: 60,
-    marginTop: '12%',
+    marginTop: '10%',
+    marginBottom: '-3%',
+    fontSize: 8,
+    color: 'grey',
+  },
+  buttonContainer: {
+    marginTop: '15%',
+    alignItems: 'center',
+    width: '100%',
+    justifyContent: 'center',
+  },
+  buttonStyle: {
+    height: 50,
+    width: '85%',
+    borderRadius: 25,
   },
 });
