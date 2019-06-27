3b99559a22dbfd7596b48f9afe1f6348295df1bc
Harpreet Somel <somel.harpreet@gmail.com>
2019-06-27 16:43:03 -0700

    successfully implemented likes feature

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -55,7 +55,7 @@ export default function HomeScreen(props) {
         likes: 0,
         comments: 0,
         retweets: 0,
-        usersLiked: [user.displayName, 'john', 'jesus', 'mary'],
+        usersLiked: {},
       })
       .then(data => {
         //success callback
--- client/src/screens/UserProfileScreen.js
+++ client/src/screens/UserProfileScreen.js
@@ -14,17 +14,35 @@ import Icon from 'react-native-vector-icons/Ionicons';
 export default function UserProfileScreen(props) {
   // Initial State
   const [feedData, setFeedData] = useState([]);
+  const [user, setUser] = useState(null);
+  //Initial database references
+  const postsRef = firebase.database().ref('posts/');
+  const verifyRef = firebase
+    .database()
+    .ref('people/')
+    .child('users');
 
   useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+      verifyRef
+        .orderByChild('userID')
+        .equalTo(user.uid)
+        .once('value', snapshot => {
+          setUser(user);
+        });
+    });
     getItems();
+
+    return () => {
+      if (unsubscribe) unsubscribe();
+      console.log('firebase listener unmounted from userProfile screen');
+    };
   }, []);
 
+  // function called to get all posts
   const getItems = () => {
     const twitPosts = [];
-    const query = firebase
-      .database()
-      .ref('posts/')
-      .limitToLast(100);
+    const query = postsRef.limitToLast(100);
     query
       .once('value', snapshot => {
         snapshot.forEach(data => {
@@ -36,25 +54,71 @@ export default function UserProfileScreen(props) {
       });
   };
 
+  // function called when user likes a post
   const handleLikePress = (item, key) => {
-    const likeRef = firebase.database().ref('posts/' + key);
-    likeRef.child('likes').transaction(current_value => {
+    const userLikedRef = postsRef.child(key).child('usersLiked');
+
+    userLikedRef
+      .orderByChild('user_name')
+      .equalTo(user.displayName)
+      .once('value', snapshot => {
+        if (!snapshot.val()) {
+          // means user has not liked post
+          console.log('we are here');
+          increaseLikeByOne(key);
+          addUserToLikesArray(key);
+        } else {
+          console.log('now we are here');
+          decreaseLikeByOne(key);
+          removeUserFromLikesArray(key);
+          console.log('user has aleady liked post');
+        }
+      });
+  };
+
+  const increaseLikeByOne = key => {
+    const increaseLikeRef = postsRef.child(key).child('likes');
+    increaseLikeRef.transaction(current_value => {
       return (current_value || 0) + 1;
     });
+  };
 
-    const whoLikes = [];
-    likeRef
-      .child('usersLiked')
-      .once('value', snapshot => {
-        snapshot.forEach(data => {
-          whoLikes.push(data.val());
-        });
+  const decreaseLikeByOne = key => {
+    const decreaseLikeRef = postsRef.child(key).child('likes');
+    decreaseLikeRef.transaction(current_value => {
+      return (current_value || 0) - 1;
+    });
+  };
+
+  const addUserToLikesArray = key => {
+    const addUserRef = postsRef.child(key).child('usersLiked');
+    addUserRef
+      .push({
+        user_name: user.displayName,
       })
-      .then(() => {
-        console.log('whoLikes........', whoLikes);
+      .then(data => {
+        console.log('successfully added ');
+      })
+      .catch(error => {
+        console.log('error ', error);
       });
+  };
 
-    console.log('heart icon pressed');
+  const removeUserFromLikesArray = key => {
+    const removeUserRef = postsRef
+      .child(key)
+      .child('usersLiked')
+      .orderByChild('user_name')
+      .equalTo(user.displayName);
+
+    removeUserRef
+      .remove()
+      .then(data => {
+        console.log('successfully removed ');
+      })
+      .catch(error => {
+        console.log('error ', error);
+      });
   };
 
   const renderSeparator = () => {
@@ -91,44 +155,16 @@ export default function UserProfileScreen(props) {
                   </View>
                 </TouchableHighlight>
               </View>
-
               <View style={styles.secondaryContainer}>
-                <View
-                  style={{
-                    backgroundColor: '#ff7f50', // light orange
-                    height: '100%',
-                    width: '100%',
-                    flex: 1,
-                  }}
-                >
+                <View style={styles.usernameContainer}>
                   <Text style={styles.usernameText}>
                     {item._value.username}
                   </Text>
                 </View>
-
-                <View
-                  style={{
-                    backgroundColor: '#00ff00', // lime green
-                    height: '100%',
-                    width: '100%',
-                    flex: 1,
-                  }}
-                >
+                <View style={styles.tweetBodyContainer}>
                   <Text style={styles.tweetText}>{item._value.text}</Text>
                 </View>
-
-                <View
-                  style={{
-                    backgroundColor: 'red',
-                    height: '100%',
-                    width: '100%',
-                    flexDirection: 'row',
-                    paddingTop: 15,
-                    paddingBottom: 5,
-                    flex: 1,
-                    justifyContent: 'space-evenly',
-                  }}
-                >
+                <View style={styles.footerContainer}>
                   <TouchableHighlight>
                     <View style={styles.iconContainer}>
                       <Icon name="md-chatboxes" size={20} />
@@ -172,7 +208,7 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: '#8acafe',
+    // backgroundColor: '#8acafe',
   },
   secondaryContainer: {
     flex: 5,
@@ -180,7 +216,7 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
     paddingLeft: 5,
-    backgroundColor: '#cccccc',
+    // backgroundColor: '#cccccc',
   },
   usernameText: {
     fontWeight: 'bold',
@@ -208,10 +244,32 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#ff3333',
   },
+  usernameContainer: {
+    // backgroundColor: '#ff7f50', // light orange
+    height: '100%',
+    width: '100%',
+    flex: 1,
+  },
+  tweetBodyContainer: {
+    // backgroundColor: '#00ff00', // lime green
+    height: '100%',
+    width: '100%',
+    flex: 1,
+  },
+  footerContainer: {
+    // backgroundColor: 'red',
+    height: '100%',
+    width: '100%',
+    flexDirection: 'row',
+    paddingTop: 15,
+    paddingBottom: 5,
+    flex: 1,
+    justifyContent: 'space-evenly',
+  },
   iconContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: 'purple',
+    // backgroundColor: 'purple',
     height: '100%',
     width: '100%',
     flex: 1,
