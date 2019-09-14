948f62960049f3daf5bc19cf5c2a2b8b6f564d31
hssomel <somel.harpreet@gmail.com>
2019-09-14 16:18:45 -0700

    modified 'YouTubeVideo.js' component

--- client/src/components/YouTubeVideo.js
+++ client/src/components/YouTubeVideo.js
@@ -1,55 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import YouTube from 'react-native-youtube';
-import firebase from 'react-native-firebase';
-import { Avatar } from 'react-native-elements';
 
-export default function YouTubeVideo(props) {
-  const {
-    style,
-    userKey,
-    otherUserKey,
-    newURL,
-    newStartTime,
-    navigateToVideo,
-    fromSettings,
-  } = props;
+// This component used in screen: SetUpProfileVideo.js
+// And component used in components: UserProfileFeedHeader.js & OtherUserProfileHeader.js
+const YouTubeVideo = props => {
+  const { style, url, startTime } = props;
   // Initial State
   const [youtubeRef, setYoutubeRef] = useState(null);
-  const [finalURL, setFinalURL] = useState(null);
-  const [startTime, setStartTime] = useState(null);
   const [isLoaded, setIsLoaded] = useState(null);
   const [ready, setReady] = useState(false);
-  const [videoExists, setVideoExists] = useState(null);
-
-  // Firebase References
-  const userRef = firebase
-    .database()
-    .ref('people')
-    .child('users');
 
   const onVideoLoaded = () => {
-    youtubeRef.seekTo(newStartTime || startTime);
+    youtubeRef.seekTo(startTime || 1);
     setReady(true);
   };
 
   useEffect(() => {
-    const key = otherUserKey || userKey;
-    const ref = userRef.child(key);
-    ref.child('youtubeURL').once('value', snapshot => {
-      if (!snapshot.val()) {
-        setVideoExists(false);
-        setIsLoaded(true);
-      } else {
-        setFinalURL(snapshot.val());
-        setVideoExists(true);
-      }
-    });
-    ref.child('startTime').once('value', snapshot => {
-      setStartTime(snapshot.val());
-      setIsLoaded(true);
-    });
-
+    setIsLoaded(true);
     return () => {
       setIsLoaded(false);
     };
@@ -58,10 +26,9 @@ export default function YouTubeVideo(props) {
   useEffect(() => {
     const interval = setInterval(() => {
       if (youtubeRef) {
-        youtubeRef.seekTo(newStartTime || startTime);
+        youtubeRef.seekTo(startTime);
       }
     }, 17000);
-
     return () => {
       clearInterval(interval);
     };
@@ -69,14 +36,14 @@ export default function YouTubeVideo(props) {
 
   return (
     <View style={{ height: '100%', width: '100%' }}>
-      {isLoaded && (finalURL || newURL) && (
+      {isLoaded && url && (
         <YouTube
           ref={component => {
             _youTubeRef = component;
             setYoutubeRef(component);
           }}
           apiKey="AIzaSyBe-XcmmWplQm2rIwqa17Cy27_bA6Z0Zvw"
-          videoId={newURL || finalURL}
+          videoId={url}
           play={true}
           fullscreen={false}
           controls={1}
@@ -85,18 +52,8 @@ export default function YouTubeVideo(props) {
           onReady={onVideoLoaded}
         />
       )}
-
-      {isLoaded && !videoExists && !fromSettings && (
-        <Avatar
-          size="large"
-          icon={{ name: 'md-add-circle', type: 'ionicon', size: 72 }}
-          containerStyle={{
-            height: '100%',
-            width: '100%',
-          }}
-          onPress={navigateToVideo}
-        />
-      )}
     </View>
   );
-}
+};
+
+export default YouTubeVideo;
